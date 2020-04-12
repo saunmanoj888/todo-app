63144c8b2c0f0c5da50878f6b7494eb8f3d8f234
saunmanoj888 <saunmanoj628@gmail.com>
2020-04-12 22:44:16 +0530

    initial commit of todo app

---
+++ app.js
@@ -0,0 +1,56 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = (todo) => {
+	const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="fas fa-trash-alt delete"></i>
+        </li>
+    `;
+	list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const todo = addForm.add.value.trim();
+	if (todo.length) {
+		generateTemplate(todo);
+		addForm.reset();
+	}
+	console.log(todo);
+});
+
+// delete todos
+list.addEventListener('click', (e) => {
+	if (e.target.classList.contains('delete')) {
+		e.target.parentElement.remove();
+	}
+});
+
+const filterTodos = (term) => {
+	Array.from(list.children)
+		.filter((todo) => {
+			return !todo.textContent.includes(term);
+		})
+		.forEach((todo) => {
+			console.log(todo);
+			todo.classList.add('filtered');
+		});
+
+	Array.from(list.children)
+		.filter((todo) => {
+			return todo.textContent.includes(term);
+		})
+		.forEach((todo) => {
+			console.log(todo);
+			todo.classList.remove('filtered');
+		});
+};
+
+// keyup event
+search.addEventListener('keyup', () => {
+	const term = search.value.trim();
+	filterTodos(term);
+});
